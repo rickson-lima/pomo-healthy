c6ca5a3001016074d6ba206f3f3b527ae0977a6c
Rickson Lima <lima.rickson20@gmail.com>
2021-02-23 21:38:26 -0400

    Aula 02 - Next + Hooks e estilização da index page

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
+
+import styles from "../styles/pages/Home.module.css";
+
+import Head from "next/Head";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Início | Move.it</title>
+      </Head>
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+        <div></div>
+      </section>
+    </div>
+  );
+}
