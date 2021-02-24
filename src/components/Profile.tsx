bbdc9fdb50bc394df96bc233d4d0bb2cac09be35
Rickson Lima <lima.rickson20@gmail.com>
2021-02-23 21:47:26 -0400

    Aula 02 - atualizando a profile image

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,7 +3,7 @@ import styles from "../styles/components/Profile.module.css";
 export function Profile() {
   return (
     <div className={styles.profileContainer}>
-      <img src="https://github.com/diego3g.png" alt="Rickson Lima" />
+      <img src="https://github.com/rickson-lima.png" alt="Rickson Lima" />
       <div>
         <strong>Rickson Lima</strong>
         <p>
